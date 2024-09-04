1b7b392e32562622c12b1e9dbc83d96aca9ab671
domshi <90562421+domshi@users.noreply.github.com>
2024-09-04 20:39:42 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+//spcific function for printing text to the console 
+//Dom, Shriver 
+//COP-2800-93446
+//23/08/2024 - 16:07
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello, I am Dom.");
+    System.out.println("My major is Computer Science.");
+    System.out.println("This is my first year of HCC, my second year starts spring semester.");
+    System.out.println("");
+    System.out.println("My goals are limited to what I am learning, right now I aim");
+    System.out.println("to learn how all programs work and how to modify them.");
+    System.out.println("");
+    System.out.println("A fun fact about me is that I am a twitch streamer.");
+  } // text to print 
+}
